1534ea220b3a91ebd19ace4057e0731b813f4fcd
meekcodes1 <mdaugirdas2@gmail.com>
2023-10-21 18:32:00 -0500

    styling and hovertext

--- static/js/index_script.js
+++ static/js/index_script.js
@@ -19,115 +19,152 @@ fetch('http://localhost:5000/get_data')
     console.error('Error:', error);
   });
 
-  function barMap(chart) {
-    let labels2 = ['Ericeira', 'Battle-Creek', 'Houston', 'Tucson', 'Los-Angeles', 'Lubbock'];
-    let data2 = [96, 70, 69, 67, 66, 64];
-    let colors2 = ['#49A9EA', '#36CAAB', '#34495E', '#B370CF', '#AC5353', '#CFD4D8'];
-    let myChart2 = document.getElementById("myChart2").getContext('2d');
-    let chart2 = new Chart(myChart2, {
-        type: 'bar',
-        data: {
-            labels: labels2,
-            datasets: [{
-                data: data2,
-                backgroundColor: colors2
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    title: {
-                        display: true,
-                        text: 'Mbps'
+
+// BAR CHART FUNCTION
+// Creating bar chart for the top 6 cities by internet speed
+function barMap(chart) {
+  let labels2 = ['Ericeira', 'Battle-Creek', 'Houston', 'Tucson', 'Los-Angeles', 'Lubbock'];
+  let data2 = [96, 70, 69, 67, 66, 64];
+  let colors2 = ['#49A9EA', '#36CAAB', '#34495E', '#B370CF', '#AC5353', '#a2e29f'];
+  let myChart2 = document.getElementById("myChart2").getContext('2d');
+  let chart2 = new Chart(myChart2, {
+      type: 'bar',
+      data: {
+          labels: labels2,
+          datasets: [{
+              data: data2,
+              backgroundColor: colors2
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true,
+                  title: {
+                      display: true,
+                      text: 'Mbps',
+                      font: {
+                        size: 24,
+                      }
+                  },
+                  ticks: {
+                    font: {
+                        size: 20,
                     }
                 }
-            },
-            plugins: {
-                title: {
-                    text: "Top 6 cities by Internet Speed",
-                    display: true
-                },
-                legend: {
-                  display: false
-              }}
-        }
-    });
+              },
+              x: {
+                ticks: {
+                    font: {
+                        size: 24,
+                    }
+                }
+            }
+          },
+          plugins: {
+              title: {
+                  text: "Top 6 cities by Internet Speed",
+                  display: true,
+                  font: {
+                    size: 24,
+                    family: 'Arial',
+                  }
+              },
+              legend: {
+                display: false
+            }}
+      }
+  });
 }
 
-  // Plotly Map
-  function populateMap(input) {
-  
-    dict = {};
-  
-    // For each row, iterate over all of the rows (i by j) to average the internet speed by country
-    for(let i = 0; i < input.length; i++) {
-      sumCountry = 0;
-      countryCount = 0;
 
-      for(let j = 0; j < input.length; j++) {
-        if(input[i]['country'] == input[j]['country']) {
-          sumCountry = sumCountry + input[j]['internet_speed'];
-          countryCount = countryCount + 1;
-        }
+// CHOROPLETH FUNCTION
+// Function to create heatmap for average internet speed by country
+function populateMap(input) {
+
+  dict = {};
+
+  // For each row, iterate over all of the rows (i by j) to average the internet speed by country
+  for(let i = 0; i < input.length; i++) {
+    sumCountry = 0;
+    countryCount = 0;
+
+    for(let j = 0; j < input.length; j++) {
+      if(input[i]['country'] == input[j]['country']) {
+        sumCountry = sumCountry + input[j]['internet_speed'];
+        countryCount = countryCount + 1;
       }
-      dict[input[i]['country']] = Math.round((sumCountry / countryCount)*100)/100;
     }
-  
-    // heatmap data
-    let data = [{
-        type: 'choropleth',
-        locationmode: 'country names',
-        locations: Object.keys(dict),
-        z: Object.values(dict),
-        colorscale: [
-          [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
-          [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
-          [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']
-        ],
-        colorbar: {
-          title: 'Mbps',
-          thickness: 20},
-        autocolorscale: false,
-        reversescale: true
-    }];
-  
-    // heatmap layout
-    let layout = {
-      title: 'Average Internet Speed By Country (Mbps)',
-      autosize: false,
-      width: 1000,
-      height: 600,
-      geo: {
-          projection: {
-              type: 'robinson'
-          }
-      }
-    };
-    // Plot the results
-    Plotly.newPlot("heatmap", data, layout, {showLink: false});
-    };
+    dict[input[i]['country']] = Math.round((sumCountry / countryCount)*100)/100;
+  }
 
+  // heatmap data
+  let data = [{
+      type: 'choropleth',
+      locationmode: 'country names',
+      locations: Object.keys(dict),
+      z: Object.values(dict),
+      colorscale: "Greens",
+      colorbar: {
+        title: 'Mbps',
+        thickness: 20},
+      autocolorscale: false,
+      reversescale: true
+  }];
+
+  // heatmap layout
+  let layout = {
+    title: {
+      text:'<b>Average Internet Speed by Country (Mbps)</b>',
+      font: {
+        family: 'Arial',
+        size: 24
+      }},
+    autosize: false,
+    width: 1000,
+    height: 600,
+    geo: {
+        projection: {
+            type: 'robinson'
+        }
+    }
+  };
+  // Plot the results
+  Plotly.newPlot("heatmap", data, layout, {showLink: false});
+  };
+
+
+// SCATTER PLOT FUNCTION
+// Function to create scatter plot of cost by city
   function make_plot(data){
       //scatter plot
           var scatter_plot = [{
               x: data.map(row => row.cost_for_nomad_in_usd),
               y: data.map(row => row.region),
-              mode: 'markers+text',
+              mode: 'markers',
               marker: {
                   size: 10,
+                  color: 'rgb(159, 125, 188)',
               },
+              text: data.map(row => row.name),
               type: 'scatter',
               name: "Digital Nomads"
           }];
           var layout = {
-              title: 'Cost for Digital Nomads Based on Regions',
+              title: {
+                text:'<b>Cost for Digital Nomads Based on Regions</b>',
+                font: {
+                  family: 'Arial',
+                  size: 24
+                }},
               xaxis: {title: "Cost(USD)"},
               yaxis: {automargin: true}
           };
           Plotly.newPlot('plot',scatter_plot,layout);
     };
 
+
+// Function to populate the dropdown box
 function populateDropdown(data) {
   data.sort((a, b) => a.name.localeCompare(b.name));
   let dropdown = d3.select("#selDataset");
@@ -138,17 +175,23 @@ function populateDropdown(data) {
   });
 }
 
+
+// Function to handle changes to the dropdown box selection
 function optionChanged(selectedValue) {
   console.log("Selected value: " + selectedValue);
   update_MetaData(selectedValue);
 }
 
+
+// Initial function
 function init() {
   if (data && data.length > 0) {
     optionChanged(data[0].name + ", " + data[0].country);
   }
 }
 
+
+// Function to populate the desination details information box
 function update_MetaData(selectedValue) {
   let selectedSample = data.find(sample => sample.name + ", " + sample.country === selectedValue);
   let MetadataDiv = d3.select("#sample-metadata");
